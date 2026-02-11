c7173f79983ea2042ae317b61e014502340ffaf5
CrochsDevs <lester.s.rodriguez.211@gmail.com>
2026-02-11 19:31:00 +0800

    Fix: Remove ghost import and clean Destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -5,7 +5,6 @@ const Destination = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Navbar />
-      
       <main className="flex-grow flex items-center justify-center pt-20">
         <div className="text-center">
           <h1 className="text-5xl font-bold text-[#2d3436] tracking-tight">
@@ -14,9 +13,6 @@ const Destination = () => {
           <p className="mt-4 text-gray-500 font-medium text-lg">
             This page is currently under development.
           </p>
-          <div className="mt-8">
-            <div className="w-16 h-1 bg-[#ffcc00] mx-auto rounded-full"></div>
-          </div>
         </div>
       </main>
     </div>
